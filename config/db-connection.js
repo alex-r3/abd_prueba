fc3f2b01a6a43ac2d71c4aced3b9ab12afffddea
Alexie <none>
2026-01-13 13:52:51 -0500

    MVP, functions added and first functional version

--- config/db-connection.js
+++ config/db-connection.js
@@ -5,9 +5,9 @@ oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 // Configuración de conexión
 const dbConfig = {
-    user: "system",
-    password: "jacoltaa",
-    connectString: "localhost:1521/XE",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    connectString: process.env.DB_CONNECTION_STRING,
     poolMin: 2,
     poolMax: 5,
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const { initializeConnection } = require("../config/db-connection");
 
